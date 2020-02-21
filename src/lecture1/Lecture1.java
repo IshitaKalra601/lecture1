8598d4c6808a23b7da8f96a0779d774bca423431
IshitaKalra601 <kalraishita2001@gmail.com>
2020-02-20 23:34:40 -0500

    I have committed

---
+++ src/lecture1/Lecture1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lecture1;
+import java.util.Scanner;
+/**
+ *
+ * @author jyoti
+ */
+public class Lecture1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+       
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the radius");
+        double r = input.nextDouble();
+        lecture1class a2 = new lecture1class(r);
+        System.out.println(a2.areaCircle());
+         
+    }
+    
+}
---
+++ src/lecture1/lecture1class.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lecture1;
+
+/**
+ *
+ * @author jyoti
+ */
+public class lecture1class {
+    public double radius;
+    public lecture1class(double r){
+        radius = r;
+    }
+    public double areaCircle(){
+        return 3.14*radius*radius;
+        
+        
+    }
+    
+}
